515e38a3dd6616badb4ded7ac2313d61066957b0
fridayraju10 <raj.friday@gmail.com>
2026-02-13 10:24:49 +0530

    DEPLOY_MSG.py

--- DEPLOY_MSG.py
+++ DEPLOY_MSG.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from twilio.rest import Client
 
 # Your Twilio credentials (TEMPORARY for local testing)
-account_sid = os.getenv("AC78393198e6d66cdd374d1120e73b45f2")
-auth_token = os.getenv("b766a80bb3851516918cc359343b63f7")
+account_sid = os.environ("TWILIO_ACCOUNT_SID")
+auth_token = os.environ("TWILIO_AUTH_TOKEN")
 
 client = Client(account_sid, auth_token)
 
@@ -42,4 +42,5 @@ if 2 <= current_hour <= 12:
         print(f"Message sent to {number}")
 else:
     print("Outside scheduled time window.")
+
 
